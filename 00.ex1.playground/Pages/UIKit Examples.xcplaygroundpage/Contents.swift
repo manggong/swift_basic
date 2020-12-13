00ab0a0ccf1f7d172c66459ec74bbdaeb8b29e18
manggong <jihwank1995@gmail.com>
2020-12-13 20:03:52 +0900

    create basic form

---
+++ 00.ex1.playground/Pages/UIKit Examples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let myLabel = UILabel(frame: CGRect(x: 0, y:0, width: 200, height: 50))
+
+
+myLabel.backgroundColor = UIColor.red
+myLabel.text = "Hello World"
+myLabel.textAlignment = .center
+myLabel.font = UIFont(name: "Georgia", size: 24)
+myLabel
+
+
---
+++ 00.ex1.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print("Welcome to Swift")
+
+var x = 10
+
+for i in 1...20 {
+    let y = i * x
+    x -= 1
+    print(y)
+}
