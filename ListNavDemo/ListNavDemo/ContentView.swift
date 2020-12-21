c1d00ca93074391a6c6f65d2212871ded707d991
manggong <jihwank1995@gmail.com>
2020-12-21 22:20:33 +0900

    init ListNavDemo

---
+++ ListNavDemo/ListNavDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListNavDemo
+//
+//  Created by 김지환 on 2020/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
