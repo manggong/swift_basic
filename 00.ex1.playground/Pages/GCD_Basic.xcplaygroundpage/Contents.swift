83bbc6897aeb2fd7044120a1e48ae91221b803c2
manggong <jihwank1995@gmail.com>
2021-01-23 08:09:53 +0900

    add main Queue

---
+++ 00.ex1.playground/Pages/GCD_Basic.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var str = "Hello, playground"
+
+DispatchQueue.main.async {
+    
+    let view = UIView()
+    view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+}
